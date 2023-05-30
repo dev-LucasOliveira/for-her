ad69c212a569e1231c6cad0e0269fb826d8b2ed0
lucas oliveira <lucas140101@gmail.com>
2023-05-30 11:23:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function sorry() {
+    alert('Lamentamos Julia Menezes, mas essa opção não está disponível para você desde o dia 25/03. Apenas resta para você aceitar casar com o Lucas Oliveira.')
+}
+
+function congrats() {
+    alert('Parabéns pela sua escolha Julia Menezes, o casamento de vocês será marcado em breve, fique de olho no seu email para mais detalhes.')
+}
